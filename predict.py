281fdff997a13dfd7fa172d0010eaa416ae6b4e6
Nick Celestin <nick@nickcelestin.com>
2022-06-14 16:38:45 -0400

    changed

--- predict.py
+++ predict.py
@@ -1,10 +1,11 @@
 from cog import BasePredictor, Path, Input, File
 import sys, os
+import uuid
 sys.path.insert(1, './backend')
 from backend.consts import ModelSize
 from backend.dalle_model import DalleModel
 from io import BytesIO
-
+import typing
 
 dalle_model = None
 class Predictor(BasePredictor):
@@ -18,15 +19,18 @@ class Predictor(BasePredictor):
     def predict(self,
                 prompt: str = Input(description="Image prompt"),
                 num: int = Input(description="Number of images to generate", default=1)
-                ) -> Path:
+                ) -> typing.Iterator[Path]:
         """Run a single prediction on the model"""
-        # ... pre-processing ...
+        import time
+        start_time = time.time()
+
         print("generating images")
         generated_imgs = self.model.generate_images(prompt, num)
         image = None
         for img in generated_imgs:
-            buffered = BytesIO()
-            img.save('output.png')
-            print(os.popen("ls").read())
-            image = img
+            img_name = uuid.uuid4()
+            img.save(f'{img_name}.png')
+            yield Path(f'{img_name}.png')
+        execution_time = (time.time() - start_time)
+        print('Execution time in seconds: ' + str(execution_time))
         return Path('output.png')
