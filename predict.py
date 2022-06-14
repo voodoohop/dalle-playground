df96e3256e9313383b4a8311fecbc0ba8fd4031b
Nick Celestin <nick@nickcelestin.com>
2022-06-14 16:58:27 -0400

    changed

--- predict.py
+++ predict.py
@@ -19,12 +19,12 @@ class Predictor(BasePredictor):
     def predict(self,
                 prompt: str = Input(description="Image prompt"),
                 num: int = Input(description="Number of images to generate", default=1),
-                model_size: ModelSize = Input(description="Size of the model", default=ModelSize.MINI, choices=[ModelSize.MINI, ModelSize.MEGA, ModelSize.MEGA_FULL])
+                model_size: str = Input(description="Size of the model", default=ModelSize.MINI, choices=[ModelSize.MINI, ModelSize.MEGA, ModelSize.MEGA_FULL])
                 ) -> typing.Iterator[Path]:
         """Run a single prediction on the model"""
         import time
         start_time = time.time()
-        self.model = DalleModel(model_size)
+        self.model = DalleModel(ModelSize[model_size])
         print("generating images")
         generated_imgs = self.model.generate_images(prompt, num)
         image = None
